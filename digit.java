7bcb7bc461397d8b93ed26f83a1f7c9ae12b4c95
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2019-11-20 19:53:37 -0800

    Solved Digit Reassembly

---
+++ digit.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class digit {
+    static Scanner in;
+
+    static String digit;
+    static int n; 
+    public static void main(String args[]) throws IOException {
+        in = new Scanner(new File("test/digit.in"));
+        for(int i = 0; i < 5; i++){
+            try {
+                init();
+                System.out.print(i + 1 + ": ");
+                solve();
+            }catch(Exception e){
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+        }
+        in.close();
+    }
+
+    private static void init(){
+        digit = in.next();
+        n = in.nextInt();
+    }
+
+    private static void solve(){
+        long sum = 0l;
+        for(int i = 0; i < digit.length() - n + 1; i++){
+                String d = digit.substring(i, i + n);
+                long num = Long.parseLong(d);
+                sum += num;
+        }
+        
+        System.out.println(sum);
+    }
+}
