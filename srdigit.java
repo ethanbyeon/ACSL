97269f33865a1658395652a81dbe87f3ba838915
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2019-11-27 19:58:27 -0800

    Solved Senior Digit Reassembly

---
+++ srdigit.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class srdigit {
+    static Scanner in;
+    static String s;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        in = new Scanner(new File("test/srdigit.in"));
+        for(int i = 0; i < 5; i++) {
+            try {
+                init();
+                System.out.print(i + 1 + ": ");
+                solve();
+            }catch(Exception e) {
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+        }
+        in.close();
+    }
+
+    static void init() {
+        String[] a = (in.nextLine()).split(" ");
+        s = a[0];
+        n = Integer.parseInt(a[1]);
+    }
+
+    static String solve() {
+        int sum = 0;
+        for(int i = 0; i < s.length(); i+=n){
+            if(i + n > s.length()){
+                int num = (i + n) - s.length();
+                for(int j = 0; j < num; j++){
+                    s += "0";
+                } 
+            }
+        }
+
+        for(int i = 0; i < s.length(); i+=n){
+            sum += Integer.parseInt(s.substring(i, i + n));
+        }
+
+        System.out.println(sum);
+        return "";
+    }
+}
